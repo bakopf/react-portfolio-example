4006d694b17ccee9b2bb7c7e303c64ab338a6fe4
Bastian.Kopf <bastian.kopf@outlook.de>
2024-04-27 15:37:01 +0200

    added welcome section elements, content and styling

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDesktop, faLaptopCode, faMobileAlt, faCode } from '@fortawesome/free-solid-svg-icons';
+
 
 function Welcome() {
   return (
@@ -6,8 +9,36 @@ function Welcome() {
       <div className="container">
         <div className="row">
           <div className="col-lg-6">
-            <h1>Welcome to Your Website</h1>
-            <p>Your welcome text goes here.</p>
+            <div className="text-container">
+              <h1> Welcome to Your Website</h1>
+              <p> Your welcome text goes here.</p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="box-container d-flex justify-content-between">
+              <div className="box text-center bg-transparent rounded">
+                <FontAwesomeIcon icon={faDesktop} className="icon orange-text mb-2" />
+                <h3>Headline</h3>
+                <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</p>
+              </div>
+              <div className="box text-center bg-transparent rounded">
+                <FontAwesomeIcon icon={faLaptopCode} className="icon orange-text mb-2" />
+                <h3>Headline Lorem</h3>
+                <p>Quid igitur dubitamus in tota eius</p>
+              </div>
+              <div className="box text-center bg-transparent rounded">
+                <FontAwesomeIcon icon={faMobileAlt} className="icon orange-text mb-2" />
+                <h3>Headline Example</h3>
+                <p>Venit ad extremum</p>
+              </div>
+              <div className="box text-center bg-transparent rounded">
+                <FontAwesomeIcon icon={faCode} className="icon orange-text mb-2" />
+                <h3>This is a longer headline</h3>
+                <p>Qui ita affectus - beatum esse</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
