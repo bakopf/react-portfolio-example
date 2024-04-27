ed47ce4e3f615cae07e0ffd0ae867076238b32ad
Bastian.Kopf <bastian.kopf@outlook.de>
2024-04-27 15:46:56 +0200

    cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ import { faFacebook, faTwitter, faLinkedin, faInstagram } from '@fortawesome/fre
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white py-5"> {/* Added py-5 for vertical padding */}
+    <footer className="bg-dark text-white py-5">
       <Container>
         <Row>
-          <Col md={4} className="mb-4 mb-md-0"> {/* Added mb-4 for bottom margin */}
-            <h3>MyPage</h3>
-            <p>Your description goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <Col md={4} className="mb-4 mb-md-0">
+            <h3>MyPortfolio</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Col>
-          <Col md={4} className="mb-4 mb-md-0"> {/* Added mb-4 for bottom margin */}
+          <Col md={4} className="mb-4 mb-md-0">
             <h3>Useful Links</h3>
             <ul className="list-unstyled">
               <li><a href="#home" className="text-white">Home</a></li>
@@ -34,7 +34,7 @@ const Footer = () => {
         <hr />
         <Row>
           <Col md={12} className="text-center">
-            <p>&copy; 2024 MyPage. All rights reserved.</p>
+            <p>&copy; 2024 MyPortfolio. All rights reserved.</p>
           </Col>
         </Row>
       </Container>
